7631bcebbae14918e6859bd861018f064ea13cd7
Jan Hackel <jan.hackel@genopace.de>
2020-10-11 19:58:19 +0200

    removed unneeded package dependency on python3-dev

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Dependencies
-pkg_dependencies="python3-pip python3-dev virtualenv libldap2-dev libsasl2-dev libssl-dev"
+pkg_dependencies="python3-pip virtualenv libldap2-dev libsasl2-dev libssl-dev"
